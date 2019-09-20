4ce0a8c1e8180ccf7672093e896cc61ec4225578
tangxiumei <citang@7lk.com>
2019-09-20 18:14:15 +0800

    update cfg.js file

--- config/env-pre.js
+++ config/env-pre.js
@@ -1,6 +1,4 @@
 module.exports = {
-  host: '127.0.0.1',
-  port: 8087,
   api: '/api',
   publicPath: '',
   distPath: './dist',
--- config/env-test.js
+++ config/env-test.js
@@ -1,6 +1,4 @@
 module.exports = {
-  host: '127.0.0.1',
-  port: 8089,
   api: '/api',
   publicPath: '',
   distPath: './dist',
